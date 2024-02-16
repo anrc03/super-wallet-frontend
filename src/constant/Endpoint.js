cf13e8aa4f2e5f4db39095c9bed1f96dbbb5ecee
dimashadianto <dimashadianto460@gmail.com>
2024-02-16 09:57:14 +0700

    Remove all about currency converter

--- src/constant/Endpoint.js
+++ src/constant/Endpoint.js
@@ -4,7 +4,3 @@ export const REGISTER_CUSTOMER = API_BASE_URL + '/auth/register';
 export const LOGIN_ADMIN = API_BASE_URL + '/auth/login/admins';
 export const REGISTER_ADMIN = API_BASE_URL + '/auth/admins';
 export const GET_ALL_CUSTOMER = API_BASE_URL + '/customer';
-
-export const VITE_REST_COUNTRIES_API_URL = 'https://restcountries.com/v3.1/all';
-export const VITE_CURRENCY_API_URL = 'https://api.freecurrencyapi.com/v1/latest';
-export const VITE_CURRENCY_API_KEY = 'fca_live_mzQXl3N9qwkEpAPU6JUoeRpEvvB5rcVy7b2WEQx';
