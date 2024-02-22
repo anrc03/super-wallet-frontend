77ed392e2471c75066191fc1535f7ad216394e60
baskara <baskararw10@gmail.com>
2024-02-22 07:53:27 +0700

    [FIX] endpoint for using server and white list url in main

--- src/constant/Endpoint.js
+++ src/constant/Endpoint.js
@@ -1,42 +1,60 @@
-export const API_BASE_URL = 'http://localhost:8088/api';
+export const API_BASE_URL = "http://108.137.90.52:8080/api";
 
 //auth
-export const LOGIN = API_BASE_URL + '/auth/login';
+export const LOGIN = API_BASE_URL + "/auth/login";
 
 //customer
-export const REGISTER_CUSTOMER = API_BASE_URL + '/auth/register';
-export const BASE_CUSTOMER = API_BASE_URL + '/customer';
-export const updateCustomer = (id, firstName, lastName, phoneNumber, birthDate, gender, address) => {
-    return BASE_CUSTOMER + `?id=${id}&firstName=${firstName}&lastName=${lastName}&phoneNumber=${phoneNumber}&birthDate=${birthDate}&gender=${gender}&address=${address}`
-}
-export const getAllCustomerByPage = (page, size) => BASE_CUSTOMER + `?page=${page}&size=${size}`
-export const getFilteredCustomerByName = (fullName, page, size) => BASE_CUSTOMER + `?fullName=${fullName}&page=${page}&size=${size}`
+export const REGISTER_CUSTOMER = API_BASE_URL + "/auth/register";
+export const BASE_CUSTOMER = API_BASE_URL + "/customer";
+export const updateCustomer = (
+  id,
+  firstName,
+  lastName,
+  phoneNumber,
+  birthDate,
+  gender,
+  address
+) => {
+  return (
+    BASE_CUSTOMER +
+    `?id=${id}&firstName=${firstName}&lastName=${lastName}&phoneNumber=${phoneNumber}&birthDate=${birthDate}&gender=${gender}&address=${address}`
+  );
+};
+export const getAllCustomerByPage = (page, size) =>
+  BASE_CUSTOMER + `?page=${page}&size=${size}`;
+export const getFilteredCustomerByName = (fullName, page, size) =>
+  BASE_CUSTOMER + `?fullName=${fullName}&page=${page}&size=${size}`;
 
 //admin
-export const REGISTER_ADMIN = API_BASE_URL + '/auth/admins';
-export const BASE_ADMIN = API_BASE_URL + '/admins';
+export const REGISTER_ADMIN = API_BASE_URL + "/auth/admins";
+export const BASE_ADMIN = API_BASE_URL + "/admins";
 
 //currency
-export const BASE_CURRENCY = API_BASE_URL + "/currency"
-export const getBaseCurrencyUrl = (baseCurrency, date) => API_BASE_URL + `?baseCurrency=${baseCurrency}&date=${date}`;
-export const getCurrencyWithTimeSeriesUrl = (fromDate, toDate, baseCurrency) => BASE_CURRENCY + `?fromDate=${fromDate}&toDate=${toDate}&baseCurrency=${baseCurrency}`
-export const getTodaysCurrencyUrl = (baseCurrency) => BASE_CURRENCY + `/today?baseCurrency=${baseCurrency}`;
+export const BASE_CURRENCY = API_BASE_URL + "/currency";
+export const getBaseCurrencyUrl = (baseCurrency, date) =>
+  API_BASE_URL + `?baseCurrency=${baseCurrency}&date=${date}`;
+export const getCurrencyWithTimeSeriesUrl = (fromDate, toDate, baseCurrency) =>
+  BASE_CURRENCY +
+  `?fromDate=${fromDate}&toDate=${toDate}&baseCurrency=${baseCurrency}`;
+export const getTodaysCurrencyUrl = (baseCurrency) =>
+  BASE_CURRENCY + `/today?baseCurrency=${baseCurrency}`;
 
 //transaction
-export const BASE_TRANSACTION =  API_BASE_URL +  "/transactions";
-export const getTransactionByPage = (page) => `${BASE_TRANSACTION}?page=${page}`
-export const WITHDRAW = BASE_TRANSACTION + "/withdrawal"
-export const TRANSFER = BASE_TRANSACTION + "/send"
-export const GET_TRANSACTION_CUSTOMER = BASE_TRANSACTION + "/customer"
+export const BASE_TRANSACTION = API_BASE_URL + "/transactions";
+export const getTransactionByPage = (page) =>
+  `${BASE_TRANSACTION}?page=${page}`;
+export const WITHDRAW = BASE_TRANSACTION + "/withdrawal";
+export const TRANSFER = BASE_TRANSACTION + "/send";
+export const GET_TRANSACTION_CUSTOMER = BASE_TRANSACTION + "/customer";
 
 //account
-export const BASE_ACCOUNT = API_BASE_URL + "/account"
+export const BASE_ACCOUNT = API_BASE_URL + "/account";
 
 //dummy-bank
-export const BASE_DUMMY_BANK = API_BASE_URL + "/dummy-bank"
+export const BASE_DUMMY_BANK = API_BASE_URL + "/dummy-bank";
 
 //change password
-export const CHANGE_PASSWORD = API_BASE_URL + "/auth/change-password"
+export const CHANGE_PASSWORD = API_BASE_URL + "/auth/change-password";
 
 //pin
-export const CREATE_PIN = API_BASE_URL + "/auth/pin"
+export const CREATE_PIN = API_BASE_URL + "/auth/pin";
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "./App.css"
-import './Index.css'
-import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import { Provider } from 'react-redux'
-import store from './components/redux/store.js'
-import CurrencyProvider from './context/CurrencyProvider.jsx'
-import axios from 'axios'
-import { VITE_CURRENCY_API_URL, VITE_REST_COUNTRIES_API_URL } from './constant/CurrencyConverter.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
+import "./Index.css";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import { Provider } from "react-redux";
+import store from "./components/redux/store.js";
+import CurrencyProvider from "./context/CurrencyProvider.jsx";
+import axios from "axios";
+import {
+  VITE_CURRENCY_API_URL,
+  VITE_REST_COUNTRIES_API_URL,
+} from "./constant/CurrencyConverter.js";
 
 axios.interceptors.request.use((config) => {
   const WHITE_LIST_URL = [
-    "http://localhost:8088/api/auth/login/admins", 
-    "http://localhost:8088/api/auth/login", 
-    "http://localhost:8088/api/auth/register", 
-    "http://localhost:8088/api/reset-password",
-    VITE_CURRENCY_API_URL, 
-    VITE_REST_COUNTRIES_API_URL
-  ]
-  if (WHITE_LIST_URL.includes(config.url)) return config
+    "http://108.137.90.52:8080/api/auth/login/admins",
+    "http://108.137.90.52:8080/api/auth/login",
+    "http://108.137.90.52:8080/api/auth/register",
+    "http://108.137.90.52:8080/api/reset-password",
+    VITE_CURRENCY_API_URL,
+    VITE_REST_COUNTRIES_API_URL,
+  ];
+  if (WHITE_LIST_URL.includes(config.url)) return config;
   const token = JSON.parse(localStorage.getItem("user")).token;
   const auth = token ? `Bearer ${token}` : "";
   config.headers.Authorization = auth;
   return config;
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -36,5 +39,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </CurrencyProvider>
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
